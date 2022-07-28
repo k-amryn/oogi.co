2a5427dfced67a5f2aad493117ecd575cf5eba41
coekuss <45906331+coekuss@users.noreply.github.com>
2022-07-28 16:24:27 -0400

    change css background src

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -57,7 +57,7 @@
   .header {
     width: 100%;
     height: 800px;
-    background-image: url(banner.png);
+    background-image: url(/banner.png);
     background-position: bottom;
     background-repeat: no-repeat;
     background-size: cover;
